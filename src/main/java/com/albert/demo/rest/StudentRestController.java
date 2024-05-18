7591cf9ba713cbe969deabff197216cce5c7d87f
ALBERT <ishengomaalbert@gmail.com>
2024-05-18 08:44:25 +0300

    PostConstruct & Path Variables

--- src/main/java/com/albert/demo/rest/StudentRestController.java
+++ src/main/java/com/albert/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.albert.demo.rest;
 
 import com.albert.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
 
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<Student>();
+        students.add(new Student("Jack", "Harrington"));
+        students.add(new Student("Theo", "Brown"));
+        students.add(new Student("Jeff", "Hardy"));
+    }
+
+    //Get all students
     @GetMapping("/students")
     public List<Student> getStudents() {
-
-         List<Student> students = new ArrayList<Student>();
-         students.add(new Student("Jack", "Harrington"));
-         students.add(new Student("Theo", "Brown"));
-         students.add(new Student("Jeff", "Hardy"));
         return students;
-
     }
+
+    //Get a single student
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return students.get(studentId);
+    }
+
+
 }
