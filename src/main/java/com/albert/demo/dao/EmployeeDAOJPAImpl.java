3d6fb0d0e1ab1eb19452912ed204196129150cad
ALBERT <ishengomaalbert@gmail.com>
2024-05-29 07:11:45 +0300

    Employee DAO & DAO Impl

--- src/main/java/com/albert/demo/dao/EmployeeDAOJPAImpl.java
+++ src/main/java/com/albert/demo/dao/EmployeeDAOJPAImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public class EmployeeDAOJPAImpl implements EmployeeDAO{
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Autowired
     public EmployeeDAOJPAImpl(EntityManager theEntityManager) {
@@ -23,5 +23,22 @@ public class EmployeeDAOJPAImpl implements EmployeeDAO{
         TypedQuery <Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
 
         return theQuery.getResultList();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return entityManager.find(Employee.class, theId);
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return entityManager.merge(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+      Employee theEmployee = entityManager.find(Employee.class, theId);
+
+      entityManager.remove(theEmployee);
     }
 }
