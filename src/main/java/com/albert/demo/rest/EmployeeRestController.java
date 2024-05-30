728a81f773ba14c12e18275c5bc64f68d43044a2
ALBERT <ishengomaalbert@gmail.com>
2024-05-30 14:18:44 +0300

    GET Single Employee

--- src/main/java/com/albert/demo/rest/EmployeeRestController.java
+++ src/main/java/com/albert/demo/rest/EmployeeRestController.java
@@ -2,28 +2,42 @@ package com.albert.demo.rest;
 
 
 //import com.albert.demo.dao.EmployeeDAO;
+
 import com.albert.demo.entity.Employee;
 import com.albert.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-  private final EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService){
+    public EmployeeRestController(EmployeeService theEmployeeService) {
         employeeService = theEmployeeService;
     }
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null){
+            throw new RuntimeException(STR."Employee with the id \{employeeId} is not found");
+        }
+
+        return theEmployee;
     }
 }
 
