d2a1d1b6b8a03a7e9debeda71255ca0ba663a1a5
ALBERT <ishengomaalbert@gmail.com>
2024-05-30 13:34:31 +0300

    Employee Service

--- src/main/java/com/albert/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/albert/demo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.albert.demo.service;
 import com.albert.demo.dao.EmployeeDAO;
 import com.albert.demo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +18,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
